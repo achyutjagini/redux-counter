6cb93de7f1471b0de09164d538ce13c0dda2bc08
achyutjagini <achyut.jagini@gmail.com>
2023-04-28 15:29:28 +0530

    Add files via upload

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,72 @@
+/*This code is setting up a Redux store using the configureStore function from
+the @reduxjs/toolkit package.
+
+The configureStore function takes a configuration 
+object as its argument, which specifies how the store should be set up.
+
+The configuration object has a reducer property that is an object containing the 
+reducers that will be used in the store.
+
+In this case, there is only one 
+reducer specified, counterReducer, which is imported from the 
+../features/counter/counterSlice file.
+
+The counterReducer is then assigned to the counter key in 
+the reducer object.
+
+This means that any actions dispatched 
+to the counter key will be handled by the counterReducer.
+
+In Redux, actions are plain JavaScript objects that describe a change
+in the application state. When an action is dispatched, it is sent to 
+the Redux store, which passes it to the reducer function that 
+corresponds to the action's type. The reducer function then takes the
+current state and the action, and returns the new state that results
+from applying the action.
+
+In the code you provided earlier, the
+store instance has a reducer property that
+is an object containing the counterReducer function, 
+which is responsible for handling actions 
+related to the counter key.
+
+This means that any action dispatched to the counter key,
+for example by calling the increment or decrement action
+creators defined in the counterSlice object, will be handled 
+by the counterReducer function. The counterReducer function 
+will receive the current state of the counter key, along 
+with the action object, and will return the new state 
+based on the logic defined in the reducer function.
+
+
+By using the configureStore function to set up the Redux store, 
+the counterReducer
+function is automatically included as part of the overall reducer 
+function used by the store,
+
+which means that it will be called whenever 
+an action related to the counter key is dispatched, and the state of 
+the counter key will be updated accordingly.
+Finally, the configureStore function returns a Redux store 
+instance, which is assigned to the store constant using
+ES6's export statement.
+
+This allows other modules in the application
+to import and use the store instance to interact with 
+the global state managed by Redux.*/
+
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from '../features/counter/counterSlice';
 
+//doesn't hurt to have comma even if only one in here
+export const store = configureStore({
 
-export const store=configureStore({
-reducer:{
- counter:counterReducer,
+    // key-counter reducer-counterReducer
+    reducer: {
+        counter: counterReducer,
 
-}
+    }
 
 })
+
+
--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,27 +1,53 @@
-import React from 'react'
+/*This is a React functional component that uses the useSelector 
+and useDispatch hooks from the react-redux library to interact with
+the counter key in the Redux store.
 
-import { useSelector,useDispatch } from 'react-redux'
+The useSelector hook is used to select and retrieve the current value
+of the count property from the counter key in the Redux store. It takes 
+a function as its argument that receives the entire Redux store state and 
+returns the part of the state that the component needs. In this case, it
+returns the count property of the counter key in the state.
 
-import { increment,decrement } from './counterSlice'
+The useDispatch hook is used to get a reference to the dispatch function, 
+which can be used to dispatch actions to update the state of the counter 
+key in the Redux store. The increment and decrement functions are imported
+from the counterSlice.js file and are used as arguments for the dispatch 
+function to update the state by incrementing or decrementing the 
+count property of the counter key.
 
+Finally, the component returns JSX that displays the current
+value of the count property from the Redux store, along with
+buttons that dispatch the increment and decrement actions when clicked.
+*/
+import { useSelector, useDispatch } from 'react-redux'
+
+import {
+  increment,
+  decrement,
+  reset,
+  incrementByAmount
+
+}
+  from './counterSlice'
+
+import { useState } from 'react'
 
 const Counter = () => {
-   const count=useSelector((state)=> state.counter.count);
+  const count = useSelector((state) => state.counter.count);
 
-   const dispatch=useDispatch();
+  const dispatch = useDispatch();
+  return (
+    <section>
+      <p>{count}</p>
+      <div>
+        <button onClick={() => dispatch(increment())}> + </button>
+        <button onClick={() => dispatch(decrement())}> - </button>
+        <button onClick={() => dispatch(reset())}> reset </button>
 
- 
-    return (
-<section>
-    <p>{count}</p>
-   <div>
-   <button onClick={()=>dispatch(increment())}> + </button>
-   <button onClick={()=>dispatch(decrement())}> - </button>
-
-   </div>
+      </div>
 
 
-</section>
+    </section>
   )
 }
 
